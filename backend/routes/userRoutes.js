7e90b79e52e1cbb4b4461b1a2e32395a15872fdc
unknown <constancekwameabbey@gmail.com>
2023-05-24 10:31:46 +0100

    complete database setup

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,20 @@
-import express  from "express";
-const router = express.Router()
-import { authUser } from "../controllers/userController.js";
+import express from "express";
+const router = express.Router();
+import {
+  authUser,
+  registerUser,
+  logoutUser,
+  getUserProfile,
+  updateUserProfile,
+} from "../controllers/userController.js";
+import { protect } from "../middleware/authMiddleware.js";
 
-router.post("/auth", authUser)
+router.post("/", registerUser);
+router.post("/auth", authUser);
+router.post("/logout", logoutUser);
+router
+  .route("/profile")
+  .get(protect, getUserProfile)
+  .put(protect, updateUserProfile);
 
-export default router
+export default router;
--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,30 @@
 import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
+import cookieParser from "cookie-parser";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+import connectDB from "./config/db.js";
 const port = process.env.PORT || 5000;
-import userRoutes from "./routes/userRoutes.js"
+import userRoutes from "./routes/userRoutes.js";
 
-const app= express()
+connectDB();
 
-app.use("/api/users", userRoutes)
+const app = express();
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+app.use(cookieParser())
+
+app.use("/api/users", userRoutes);
 
 app.get("/", (req, res) => {
-    res.send("Server is ready")
-})
+  res.send("Server is ready");
+});
 
-app.use(notFound)
-app.use(errorHandler)
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => {
-    console.log(`Server started on port ${port}`)
-})
+  console.log(`Server started on port ${port}`);
+});
